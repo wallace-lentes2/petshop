2b1b84525f95fb685aa663d001ee9d239209f824
wallace-lentes2 <wallacelentes99@gmail.com>
2025-07-28 13:03:19 -0300

    Update vite.config.ts
    nome

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/nome-do-seu-repositorio/' : '/',
+  base: mode === 'production' ? '/petshop/' : '/',
   server: {
     host: "::",
     port: 8080,
